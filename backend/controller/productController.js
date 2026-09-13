c377c5d4d63861ea8e1e3f3902e9f178468d1baa
Muhammad Haris <sheikharis574@gmail.com>
2026-09-14 02:49:28 +0500

    Update

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,35 @@
 import Product from "../model/Product.js";
 
+
+// Add Product
 export const addProduct = async (req, res) => {
+  try {
+    const { title, description, category, price, image, stock } = req.body;
+
+    if ((!title, !description, !category, !price, !image, !stock)) {
+      return res.status(400).json({ message: "Please Enter all Fields" });
+    }
+
+    const product = await Product.create({
+      title,
+      description,
+      category,
+      price,
+      image,
+      stock,
+    });
+
+    res.status(200).json({
+      message: "Product Added Successfully",
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error Adding Product", error });
+  }
+};
+
+// Delete Product
+export const deleteProduct = async (req, res) => {
   try {
     const { title, description, category, price, image, stock } = req.body;
 
--- backend/model/Product.js
+++ backend/model/Product.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
   },
-  description: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  category: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    default: 0,
-  },
-  image: {
-    type: String,
-  },
-  stock: {
-    type: Number,
-    default: 0,
-  },
-});
+);
 
 export default mongoose.model("Product", productSchema);
