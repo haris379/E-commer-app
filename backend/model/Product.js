f699c8e7e95242d0bcca35be26b34ab5e80bdb31
Muhammad Haris <sheikharis574@gmail.com>
2026-09-14 00:11:20 +0500

    Create Product Model

---
+++ backend/model/Product.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  image: {
+    type: String,
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export default mongoose.model("Product", productSchema);
