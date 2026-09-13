8f50f28accd03fecea4cc4658fe20eecd5bdf2e5
Muhammad Haris <sheikharis574@gmail.com>
2026-09-14 02:56:08 +0500

    Craete Router for Deleet Product

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addProduct } from "../controller/productController.js";
+import { addProduct, deleteProduct } from "../controller/productController.js";
 
 const router = express.Router();
 
 router.post("/add-product", addProduct);
+router.delete("/delete/:id", deleteProduct);
 
 export default router;
