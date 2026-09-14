b289b41a37b67e559070d0ad1148b1802b65d66a
Muhammad Haris <sheikharis574@gmail.com>
2026-09-14 16:37:59 +0500

    Craete Route  for Fetch Category

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -3,6 +3,7 @@ import {
   addProduct,
   deleteProduct,
   getAllProducts,
+  getAllCategory,
 } from "../controller/productController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getAllProducts);
 router.post("/add-product", addProduct);
 router.delete("/delete/:id", deleteProduct);
+router.get("/categories", getAllCategory);
 
 export default router;
