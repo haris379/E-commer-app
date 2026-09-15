5ca22e063e1aef952691e0a466cd0f8a85b13457
Muhammad Haris <sheikharis574@gmail.com>
2026-09-15 15:59:53 +0500

    Craete Model for Cart

---
+++ backend/model/Cart.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const cartSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Cart", cartSchema);
