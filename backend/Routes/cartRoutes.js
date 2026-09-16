b85d190ae372c3f2df79aef42933e11b90a8c74c
Muhammad Haris <sheikharis574@gmail.com>
2026-09-16 19:36:38 +0500

    Add Route to decrease Quantity of product in cart

--- backend/Routes/cartRoutes.js
+++ backend/Routes/cartRoutes.js
@@ -4,6 +4,7 @@ import {
   removeItem,
   getCart,
   increaseQunatity,
+  decreaseQunatity,
 } from "../controller/cartController.js";
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post("/addToCart", addItem);
 router.post("/removeItem", removeItem);
 router.get("/:userId", getCart);
 router.put("/increase/:productId", increaseQunatity);
+router.put("/decrease/:productId", decreaseQunatity);
 
 export default router;
