ed97c7ba8d483e0fc63b902ec4a32bc77e754fc6
Muhammad Haris <sheikharis574@gmail.com>
2026-09-16 19:10:48 +0500

    Add controller and route to increase Quantity of product in cart

--- backend/Routes/cartRoutes.js
+++ backend/Routes/cartRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { addItem, removeItem, getCart } from "../controller/cartController.js";
+import {
+  addItem,
+  removeItem,
+  getCart,
+  increaseQunatity,
+} from "../controller/cartController.js";
 const router = express.Router();
 
 router.post("/addToCart", addItem);
 router.post("/removeItem", removeItem);
 router.get("/:userId", getCart);
+router.put("/increase/:productId", increaseQunatity);
 
 export default router;
--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -75,3 +75,28 @@ export const getCart = async (req, res) => {
     res.status(500).json({ message: "Error Fetching Cart", error });
   }
 };
+
+// Increase Product Quantity
+
+export const increaseQunatity = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const { productId } = req.params;
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({ message: "No Cart Found" });
+    }
+    const item = cart.items.find((p) => p.productId.toString() === productId);
+    item.quantity += 1;
+    await cart.save();
+    res.status(200).json({
+      message: "Quantity Increases",
+      item,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Increasing Quantity of Product in Cart", error });
+  }
+};
